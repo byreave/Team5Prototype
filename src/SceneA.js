80555d1adc02005cc2a6baa57b3177c279684052
Spencer Wang <GamerWang@Outlook.com>
2018-09-13 10:57:16 -0600

    Format code with prettier

--- src/SceneA.js
+++ src/SceneA.js
@@ -1,90 +1,91 @@
-
 import Player from "./Player.js";
 import Planet from "./Planet.js";
-import * as dat from "dat.gui"
+import * as dat from "dat.gui";
 import PlanetGenerator from "./PlanetGenerator.js";
 import Back from "./Back.js";
 import Moon from "./Moon.js";
-    
+
 export default class SceneA extends Phaser.Scene {
-    constructor() {
-      super();
-      this.level = 0;
+  constructor() {
+    super();
+    this.level = 0;
+  }
+
+  preload() {
+    this.load.image("sky", "assets/sky.png");
+    //this.load.image('ground', 'assets/platform.png');
+    this.load.image("star", "assets/star.png");
+    this.load.image("bomb", "assets/bomb.png");
+    // debugger
+    this.load.image("back", "assets/space.png");
+    //this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+
+    // Read sprites from the atlas
+    // this.load.atlas('gems', '_LocalAssets/gems.png', '_LocalAssets/gems.json');
+
+    this.load.image("planet1", "assets/Planet1_static.png");
+    this.load.image("planet2", "assets/Planet2_static.png");
+  }
+
+  create() {
+    // Create animation resources here
+
+    this.planets = new Array();
+    // this.add.image(400, 300, 'sky');
+    this.background = new Back(this, 0, 0.5, 100);
+    var gui = new dat.GUI();
+    this.player = new Player(this, 0, 0);
+    this.planetGenerator = new PlanetGenerator(this);
+    this.planetGenerator.AutoGenerateAScreen();
+    //for(var i = 0; i < 5; i ++)
+    //{
+    //    this.planets.push(new Planet(this, 150 + 110 * i, 50, 50, "Planet" + i.toString()));
+    //}
+    //this.moon1 = new Moon(this, this.planet1, 0.0);
+    //this.moon1.setOrbiting(true);
+    console.log(this.planets.length);
+    var f1 = gui.addFolder("Test");
+    f1.add(this.player.sprite.body.velocity, "x").listen();
+    f1.add(this.player.sprite.body.velocity, "y").listen();
+    f1.add(this.player.sprite, "x").listen();
+    f1.add(this.player.sprite, "y").listen();
+
+    f1.add(this.player, "angle").listen();
+    f1.add(this.player, "isCCW").listen();
+
+    f1.open();
+    this.cameras.main.setBounds(0, 0, 3200, 600);
+    this.make = false;
+    // console.log(this);
+  }
+
+  update(timestep, delta) {
+    this.player.update(delta);
+    this.player.orbitUpdate(delta);
+    //this.moon1.OrbitUpdate(delta);
+    for (var p of this.planets) {
+      p.update(delta);
     }
-
-    preload ()
-    {
-        this.load.image('sky', 'assets/sky.png');
-        //this.load.image('ground', 'assets/platform.png');
-        this.load.image('star', 'assets/star.png');
-        this.load.image('bomb', 'assets/bomb.png');
-        // debugger
-        this.load.image('back', 'assets/space.png');
-        //this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
-
-        // Read sprites from the atlas
-        // this.load.atlas('gems', '_LocalAssets/gems.png', '_LocalAssets/gems.json');
-
-        this.load.image('planet1', 'assets/Planet1_static.png');
-        this.load.image('planet2', 'assets/Planet2_static.png');
+    if (this.player.landedOn == this.planets.length - 3) {
+      this.make = true;
+      this.make = this.planetGenerator.GenerateAScreen(this.make);
     }
-    
-    create ()
-    {
-        // Create animation resources here
+    this.cameras.main.scrollX = this.player.sprite.x - 400;
 
-        this.planets = new Array();
-        // this.add.image(400, 300, 'sky');
-        this.background = new Back(this, 0,0.5,100);
-        var gui = new dat.GUI();
-        this.player = new Player(this, 0, 0); 
-        this.planetGenerator = new PlanetGenerator(this);
-        this.planetGenerator.AutoGenerateAScreen();
-        //for(var i = 0; i < 5; i ++)
-        //{
-        //    this.planets.push(new Planet(this, 150 + 110 * i, 50, 50, "Planet" + i.toString()));
-        //}
-        //this.moon1 = new Moon(this, this.planet1, 0.0);
-        //this.moon1.setOrbiting(true);
-        console.log(this.planets.length); 
-        var f1 = gui.addFolder('Test');
-        f1.add(this.player.sprite.body.velocity, "x").listen();
-        f1.add(this.player.sprite.body.velocity, "y").listen();
-        f1.add(this.player.sprite, "x").listen();
-        f1.add(this.player.sprite, "y").listen();
+    this.background.image.x -= this.background.velocity;
+    this.background.image2.x -= this.background.velocity;
 
-        f1.add(this.player, "angle").listen();
-        f1.add(this.player, "isCCW").listen();
-
-        f1.open();
-        this.cameras.main.setBounds(0, 0, 3200, 600);
-        this.make = false;
-        // console.log(this);
+    if (
+      this.background.image.x <=
+      -this.background.image.width + this.player.x
+    ) {
+      this.background.image.x = this.background.image2.width + this.player.x;
     }
-
-    update (timestep, delta)
-    {
-        this.player.update(delta);
-        this.player.orbitUpdate(delta);
-        //this.moon1.OrbitUpdate(delta);
-        for(var p of this.planets)
-        {
-            p.update(delta);
-        }
-        if(this.player.landedOn == this.planets.length - 3) {
-            this.make = true;
-            this.make = this.planetGenerator.GenerateAScreen(this. make);
-        }
-        this.cameras.main.scrollX = this.player.sprite.x - 400;
-
-        this.background.image.x-=this.background.velocity;
-        this.background.image2.x-=this.background.velocity;
-
-        if(this.background.image.x <= -this.background.image.width + this.player.x) {
-            this.background.image.x = this.background.image2.width + this.player.x;
-        }
-        if(this.background.image2.x <= -this.background.image2.width + this.player.x) {
-            this.background.image2.x = this.background.image.width + this.player.x ;
-        }
+    if (
+      this.background.image2.x <=
+      -this.background.image2.width + this.player.x
+    ) {
+      this.background.image2.x = this.background.image.width + this.player.x;
     }
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
-import 'phaser';
-import SceneA from "./SceneA.js"
+import "phaser";
+import SceneA from "./SceneA.js";
 var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    physics: {
-        default: 'arcade',
-        arcade:{
-            debug: true,
-            gravity:{
-                y:30
-            }
-        }
-    },
-    scene: SceneA
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+      gravity: {
+        y: 30
+      }
+    }
+  },
+  scene: SceneA
 };
 
 var game = new Phaser.Game(config);
-
