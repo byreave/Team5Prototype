31d692bba8a1a083c59fe4b55662ab0156e4683b
Spencer Wang <GamerWang@Outlook.com>
2018-09-18 11:18:52 -0600

    Remove margin

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import "phaser";
-import SceneA from "./SceneA.js";
+import 'phaser';
+import SceneA from './SceneA.js';
 var config = {
-    type: Phaser.AUTO,
-    width: 1920,
-    height: 1080,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            debug: true,
-            gravity: {
-                scale: 0
-            }
-        }
-    },
-    scene: SceneA
+	type: Phaser.AUTO,
+	width: 1920,
+	height: 1080,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			debug: true,
+			gravity: {
+				scale: 0
+			}
+		}
+	},
+	scene: SceneA
 };
 
 var game = new Phaser.Game(config);
