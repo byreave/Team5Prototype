df9a564d88d1f895a3f7363b596664297bcc7635
Mit Doshi <mit.doshi557@gmail.com>
2018-12-06 13:04:38 -0700

    Adding Credits

--- src/SceneB.js
+++ src/SceneB.js
@@ -21,6 +21,18 @@ export default class SceneB extends Phaser.Scene {
         this.add.image(960, 540, 'EndScreen');
         this.add.bitmapText(750, 200, 'd_p', "Score: " + this._score, 50, 1);
         this.add.bitmapText(1500, 950, 'd_p', "Press Start", 25, 1);
+
+        this.add.bitmapText(250, 500, 'd_p', "Credits: ", 50, 1);
+        this.add.bitmapText(100, 650, 'd_p', "Producer:", 25, 1);
+        this.add.bitmapText(350, 650, 'd_p', "Lis Moberly", 25, 1);
+        this.add.bitmapText(100, 700, 'd_p', "Artist:", 25, 1);
+        this.add.bitmapText(350, 700, 'd_p', "Dani Powers", 25, 1);
+        this.add.bitmapText(100, 750, 'd_p', "Tech-Art:", 25, 1);
+        this.add.bitmapText(350, 750, 'd_p', "Xipeng Wang", 25, 1);
+        this.add.bitmapText(100, 800, 'd_p', "Engineers:", 25, 1);
+        this.add.bitmapText(350, 800, 'd_p', "Byreave Luo", 25, 1);
+        this.add.bitmapText(350, 850, 'd_p', "Mit Doshi", 25, 1);
+        
         // console.log(this.input.keyboard.createCursorKeys());
         this.input.once('pointerdown', function (event) {
             // console.log(event);
